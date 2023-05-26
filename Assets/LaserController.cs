239904a85f197e8094d747f619907778eb334433
fst99 <96962890+fst99@users.noreply.github.com>
2023-05-26 19:48:38 +0200

    added The tripleLasershoot feature

---
+++ Assets/LaserController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class LaserController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private void Update()
+    {
+        Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
+        
+        // Check if the laser is off-screen
+        if (viewportPos.x < 0f || viewportPos.x > 1f)
+        {
+            Destroy(gameObject);
+        }
+        Move();
+    }
+    private void Move()
+    {
+        Vector3 temp = transform.position;
+        temp.x += speed * Time.deltaTime;
+        transform.position = temp;
+    }
+}
